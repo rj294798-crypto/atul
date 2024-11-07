a259fb2f1ba62118a2329129cb928a3eef7b8d64
Amine Sadali <amine.sadali@gmail.com>
2024-11-07 02:28:54 +0100

    update: change 3d sky

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 
-import skyScene from "../assets/3d/sky.glb";
+import skyScene from "../assets/3d/sky_anime.glb";
 
 // 3D Model from: https://sketchfab.com/3d-models/phoenix-bird-844ba0cf144a413ea92c779f18912042
 export function Sky({ isRotating }) {
