21de8cfea7c16e306826cd306b45bdf5ab4df229
Amine Sadali <amine.sadali@gmail.com>
2024-11-09 15:59:34 +0100

    updates: add project page intro + project image | project page in progress

--- src/constants/index.js
+++ src/constants/index.js
@@ -206,36 +206,19 @@ export const projects = [
       'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
     link: "https://github.com/adrianhajdin/threads",
   },
-  {
-    iconUrl: car,
-    theme: "btn-back-blue",
-    name: "Car Finding App",
-    description:
-      "Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.",
-    link: "https://github.com/adrianhajdin/project_next13_car_showcase",
-  },
-  {
-    iconUrl: snapgram,
-    theme: "btn-back-pink",
-    name: "Full Stack Instagram Clone",
-    description:
-      "Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.",
-    link: "https://github.com/adrianhajdin/social_media_app",
-  },
-  {
-    iconUrl: estate,
-    theme: "btn-back-black",
-    name: "Real-Estate Application",
-    description:
-      "Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.",
-    link: "https://github.com/adrianhajdin/projects_realestate",
-  },
-  {
-    iconUrl: summiz,
-    theme: "btn-back-yellow",
-    name: "AI Summarizer Application",
-    description:
-      "App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.",
-    link: "https://github.com/adrianhajdin/project_ai_summarizer",
-  },
+  /* 
+  TO DO: Generate data the think about improvement to the component to hold images
+
+  object:
+  image array
+  link array
+  description array
+
+  project list:
+  eas'Invoice '(needs screen)
+  BRAVASHOTEL Calendar ok
+  KONFIGEAR ok
+  MIRAL ok
+  LEWINNERSCLUB ok
+  */
 ];
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,54 +6,63 @@ import { arrow } from "../assets/icons";
 
 const Projects = () => {
   return (
-    <section className='max-container'>
-      <h1 className='head-text'>
+    <section className="max-container">
+      <h1 className="head-text">
         My{" "}
-        <span className='blue-gradient_text drop-shadow font-semibold'>
+        <span className="blue-orange-gradient_text drop-shadow font-semibold">
           Projects
         </span>
       </h1>
 
-      <p className='text-slate-500 mt-2 leading-relaxed'>
-        I've embarked on numerous projects throughout the years, but these are
-        the ones I hold closest to my heart. Many of them are open-source, so if
-        you come across something that piques your interest, feel free to
-        explore the codebase and contribute your ideas for further enhancements.
-        Your collaboration is highly valued!
+      <p className="text-slate-500 mt-2 leading-relaxed">
+        I’ve embarked on a variety of impactful projects throughout my career,
+        spanning multiple industries and regions. Each project has offered
+        unique challenges and invaluable learning experiences—from developing
+        advanced front-end interfaces and robust back-end systems to handling
+        some DevOps tasks that shaped me into a versatile full-stack developer.
+      </p>
+      <p className="text-slate-500 mt-2 leading-relaxed">
+        This blend of expertise enables me to quickly adapt to new technologies
+        and deliver efficient, user-centered solutions tailored to diverse
+        client needs. Discover below some of the projects I’ve had the
+        opportunity to work on:
+      </p>
+      <p className="text-slate-500 mt-2 leading-relaxed">
+        Discover below some of the projects I’ve had the opportunity to work on
       </p>
 
-      <div className='flex flex-wrap my-20 gap-16'>
+      <div className="flex flex-wrap my-20 gap-16">
         {projects.map((project) => (
-          <div className='lg:w-[400px] w-full' key={project.name}>
-            <div className='block-container w-12 h-12'>
+          <div className="lg:w-[400px] w-full" key={project.name}>
+            <div className="block-container w-12 h-12">
               <div className={`btn-back rounded-xl ${project.theme}`} />
-              <div className='btn-front rounded-xl flex justify-center items-center'>
+              <div className="btn-front rounded-xl flex justify-center items-center">
                 <img
                   src={project.iconUrl}
-                  alt='threads'
-                  className='w-1/2 h-1/2 object-contain'
+                  alt="threads"
+                  className="w-1/2 h-1/2 object-contain"
                 />
               </div>
             </div>
 
-            <div className='mt-5 flex flex-col'>
-              <h4 className='text-2xl font-poppins font-semibold'>
+            <div className="mt-5 flex flex-col">
+              <h4 className="text-2xl font-poppins font-semibold">
                 {project.name}
               </h4>
-              <p className='mt-2 text-slate-500'>{project.description}</p>
-              <div className='mt-5 flex items-center gap-2 font-poppins'>
+              <p className="mt-2 text-slate-500">{project.description}</p>
+              <div className="mt-5 flex items-center gap-2 font-poppins">
                 <Link
                   to={project.link}
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  className='font-semibold text-blue-600'
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="font-semibold text-blue-600"
                 >
                   Live Link
                 </Link>
                 <img
                   src={arrow}
-                  alt='arrow'
-                  className='w-4 h-4 object-contain'
+                  alt="arrow"
+                  className="w-4 h-4 object-contain"
                 />
               </div>
             </div>
@@ -61,7 +70,7 @@ const Projects = () => {
         ))}
       </div>
 
-      <hr className='border-slate-200' />
+      <hr className="border-slate-200" />
 
       <CTA />
     </section>
