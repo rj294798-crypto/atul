eb6f703bc967c99607b30a4625158a968260b161
Amine Sadali <amine.sadali@gmail.com>
2024-11-17 22:44:14 +0100

    fix: added basename url to fix github pages deployement

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { About, Contact, Home, Projects } from './pages';
 const App = () => {
   return (
     <main className="bg-slate-300/20">
-      <Router>
+      <Router basename="/3D_portfolio/">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
